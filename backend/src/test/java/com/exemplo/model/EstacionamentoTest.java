794dcd8152554b75fd76cee0c3faf3ed43610e7e
paulohborba <paulohbr18@gmail.com>
2025-05-07 02:43:52 -0300

    feat(tests): Adicionando arquivos de teste TDD provisorio para testes da infra

---
+++ backend/src/test/java/com/exemplo/model/EstacionamentoTest.java
@@ -0,0 +1,13 @@
+package com.exemplo.model;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MeuProjetoApplicationTests {
+
+    @Test
+    void testePrototipo() {
+        // Falha proposital
+        fail("Teste falhando propositalmente para TDD");
+    }
+}
