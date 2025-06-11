e5a666b2f362e428a57bf6db138abc024f8437e7
paulohborba <paulohbr18@gmail.com>
2025-06-11 15:14:09 -0300

    fix(infra): Mudando local do EstacionamentoApplication.java

---
+++ backend/src/main/java/com/estacionamento_tppe/EstacionamentoApplication.java
@@ -0,0 +1,15 @@
+
+package com.estacionamento_tppe; 
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstacionamentoApplication {
+
+    public static void main(String[] args) {
+        // Método main é o ponto de entrada da aplicação Java
+        SpringApplication.run(EstacionamentoApplication.class, args);
+    }
+
+}
