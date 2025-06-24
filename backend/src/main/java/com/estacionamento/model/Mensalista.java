861a2cc5ead35e5b07b50c7b71de1c77f2d37940
paulohborba <paulohbr18@gmail.com>
2025-06-24 17:48:13 -0300

    feat(backend): Adicionando a classe de Mensalista na model

---
+++ backend/src/main/java/com/estacionamento/model/Mensalista.java
@@ -0,0 +1,26 @@
+package com.estacionamento.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Entity
+@Table(name = "Mensalista")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Mensalista {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "valor", nullable = false, precision = 10, scale = 2)
+    private Double valor;
+
+    @Column(name = "periodo_meses")
+    private Integer periodoMeses;
+
+    @Column(name = "descricao", length = 255)
+    private String descricao;
+}
