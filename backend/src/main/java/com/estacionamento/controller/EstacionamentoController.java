b01085aff22016b1d02516ed68e7785722e0e614
paulohborba <paulohbr18@gmail.com>
2025-06-25 11:20:58 -0300

    fix(backend): Ajustando o arquivo EstacionamentoController.java

--- backend/src/main/java/com/estacionamento/controller/EstacionamentoController.java
+++ backend/src/main/java/com/estacionamento/controller/EstacionamentoController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/estacionamento")
+@RequestMapping("/api/estacionamentos")
 public class EstacionamentoController {
 
     private final EstacionamentoService estacionamentoService;
