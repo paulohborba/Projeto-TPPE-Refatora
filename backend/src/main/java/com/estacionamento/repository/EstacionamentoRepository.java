712a03fb50522428e5a2b8e6c2fb52b82295a3c1
paulohborba <paulohbr18@gmail.com>
2025-06-25 15:14:37 -0300

    fix(backend): Consertando EstacionamentoRepository.java

--- backend/src/main/java/com/estacionamento/repository/EstacionamentoRepository.java
+++ backend/src/main/java/com/estacionamento/repository/EstacionamentoRepository.java
@@ -4,9 +4,9 @@ import com.estacionamento.model.Estacionamento;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface EstacionamentoRepository extends JpaRepository<Estacionamento, Long> {
-
-    Object findByNome(String string);
-
+    Optional<Estacionamento> findByNome(String nome);
 }
